c52b9b6e03030b8273e2dc6209d9ad6978c20062
Jorge Martinez <113647218+whatareyoy@users.noreply.github.com>
2023-10-12 22:01:30 -0500

    // Thanking the user for their input

--- js/site.js
+++ js/site.js
@@ -3,6 +3,7 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+// Thanking the user for their input
 function Thanks(){
 	var name = document.getElementById('name');
 	var subscribe = document.getElementById('submit');
