f51d4be45a4bf26bc83c488b085345d79c364796
Jorge Martinez <113647218+whatareyoy@users.noreply.github.com>
2023-10-12 23:13:26 -0500

    comments

--- js/site.js
+++ js/site.js
@@ -5,8 +5,8 @@ jQuery(function($) {
 });
 // Thanking the user for their input
 function Thanks(){
-	var name = document.getElementById('name');
-	var subscribe = document.getElementById('submit');
+	var name = document.getElementById('name');  //var name is name of user
+	var subscribe = document.getElementById('submit');  //var subscribe is submition of form
 	
 	subscribe.innerHTML = "Thank you for joing our comunity"  + name.value;
 }
